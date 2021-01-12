0c181349e99d95d89a750f031010fb36fecc7d12
Yaksh Bariya <yakshbari4@gmail.com>
2021-01-12 12:10:37 +0530

    Fix error in math.ts

--- src/commands/math.ts
+++ src/commands/math.ts
@@ -1,6 +1,6 @@
 import * as Discord from "discord.js";
 
-xportt default function(message: Discord.Message){
+export default function(message: Discord.Message){
 		try {
 			message.channel.send(eval(message.content.replace(/\×/,"*").replace(/x/i,"*").replace(/\÷/,"/").slice(5)).toString())
 		} catch {
