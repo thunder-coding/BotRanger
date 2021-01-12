8a207d4e480b55cadc1371046e55afd9d7997649
Yaksh Bariya <yakshbari4@gmail.com>
2021-01-12 16:51:27 +0530

    Fix another error in math.ts

--- src/commands/math.ts
+++ src/commands/math.ts
@@ -2,7 +2,7 @@ import * as Discord from "discord.js";
 
 export default function(message: Discord.Message){
 		try {
-			message.channel.send(eval(message.content.replace(/\×/g,"*").replace(/x/gi,"*").replace(/\÷/g,"/").slice(5)).toString())
+			message.channel.send(eval(message.content.replace(/\×/g,"*").replace(/x/gi,"*").replace(/\÷/g,"/").slice(6)).toString())
 		} catch {
 			message.channel.send("There was an error in your mathematical expression")
 		}
