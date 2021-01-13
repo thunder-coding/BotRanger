a1fafe3c3901fe8b8e1bd50b7e1c84cf45e2b269
YAKSH BARIYA <yakshbari4@gmail.com>
2021-01-13 08:52:03 +0530

    Added help for math command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,6 +1,7 @@
 import * as Discord from "discord.js";
 
 export default function(message: Discord.Message){
+	if(message.content.match(/^-help[\w]*$/){
 		let msg = new Discord.MessageEmbed()
 			.setColor('#ffffff')
 			.setTitle("BotRanger Help")
@@ -13,4 +14,18 @@ export default function(message: Discord.Message){
 			.setFooter("Help by BotRanger")
 			
 		message.channel.send(msg)
+	}
+	else if(message.content.match(/^-help[\w]+math/){
+		let msg = new Discord.MessageEmbed()
+			.setColor('#ffffff')
+			.setTitle("BotRanger Help")
+			.addFields(
+				{ name: '`-math <expression>`', value: 'Solve a mathematical expression', inline: true },
+				{ name: 'Supported mathematical operations', value: 'Multiplication: `x` or `*`\nDivision: `/` or `÷`\nSubtraction: `-`\nAddition: `+`\nPower: `**`', inline: true }
+			)
+			.setTimestamp()
+			.setFooter("Help by BotRanger")
+			
+		message.channel.send(msg);
+	}
 }
