a37c3aef2d3c74a63f52b43823d64ca5d1956195
YAKSH BARIYA <yakshbari4@gmail.com>
2021-01-13 10:34:06 +0530

    Added exception handling in math.ts (#14)

--- src/commands/math.ts
+++ src/commands/math.ts
@@ -1,6 +1,10 @@
 import * as Discord from "discord.js";
 
 export default function(message: Discord.Message){
+		if(message.content.length < 6){
+			message.channel.send("Expected 1 or more arguements, recieved 0")
+			return;
+		}
 		try {
 			message.channel.send(eval(message.content.replace(/\×/g,"*").replace(/x/gi,"*").replace(/\÷/g,"/").slice(6)).toString())
 		} catch {
