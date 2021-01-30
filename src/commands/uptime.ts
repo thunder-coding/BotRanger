1c50941b4b1fee185ba54a00c009854a5e550951
Yaksh Bariya <yakshbari4@gmail.com>
2021-01-30 08:45:33 +0530

    Fix critical bug in uptime.ts

--- src/commands/uptime.ts
+++ src/commands/uptime.ts
@@ -1,10 +1,10 @@
 import * as Discord from "discord.js";
 
 export default function(message: Discord.Message, uptime: number): void{
-		let uptimeSeconds = Math.round((uptime/1000)%60);
-		let uptimeMinutes = Math.round((uptime/60000)%60);
-		let uptimeHours = Math.round((uptime/3600000)%24);
-		let uptimeDays = Math.round(uptime/86400000);
+		let uptimeSeconds = Math.floor((uptime/1000)%60);
+		let uptimeMinutes = Math.floor((uptime/60000)%60);
+		let uptimeHours = Math.floor((uptime/3600000)%24);
+		let uptimeDays = Math.floor(uptime/86400000);
 		
 		message.channel.send(`${uptimeDays}d ${uptimeHours}h ${uptimeMinutes}min ${uptimeSeconds}s`);
 }
