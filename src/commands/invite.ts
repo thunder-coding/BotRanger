534a329c9721e205170f76437305f913437c15ae
Yaksh Bariya <yakshbari4@gmail.com>
2021-02-15 09:36:50 +0530

    Removed invite command

--- src/commands/invite.ts
+++
@@ -1,6 +0,0 @@
-import * as Discord from "discord.js";
-
-export default function(message: Discord.Message): void{
-		message.channel.send("Thanks fir inviting BotRanger to your Discord server. :)")
-		message.channel.send("https://discord.com/oauth2/authorize?client_id=769436540329394189&scope=bot&permissions=8")
-}
