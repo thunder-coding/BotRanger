455f96681a647e26ba00939324522f9c45046c22
Yaksh Bariya <yakshbari4@gmail.com>
2021-04-13 14:46:40 +0530

    Import only types whereever possible

--- src/commands/uptime.ts
+++ src/commands/uptime.ts
@@ -1,4 +1,4 @@
-import * as Discord from 'discord.js'
+import type * as Discord from 'discord.js'
 
 export default function (message: Discord.Message, uptime: number): void {
 	let uptimeSeconds = Math.floor((uptime / 1000) % 60)
